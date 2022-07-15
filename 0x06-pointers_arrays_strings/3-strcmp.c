403c9a79ad76cd724b075d0b3c69c1edaa12797f
James Alvine <jamesalvine340@gmail.com>
2022-07-14 21:58:41 -0700

    arrays

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,18 +1,24 @@
+
 #include "main.h"
-#include <stdio.h>
 
 /**
- *main - check the code
- *
- *Return: Always 0.
+ *_strcmp - compares two strings.
+ *@s1: the first string to compare.
+ *@s2: second string to compare.
+ *Return: a number less than 0 if s1 is less than s2,
+ *0 if they are equal,
+ *a number more than 0 if s1 is greater than s2.
  */
-int main(void)
+int _strcmp(char *s1, char *s2)
 {
-	char s1[] = "Hello";
-	char s2[] = "World!";
-
-	printf("%d\n", _strcmp(s1, s2));
-	printf("%d\n", _strcmp(s2, s1));
-	printf("%d\n", _strcmp(s1, s1));
-	return (0);
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
