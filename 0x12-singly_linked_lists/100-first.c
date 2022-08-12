1a51de53786df8a1abcee7714513942190e6a22d
James Alvine <jamesalvine340@gmail.com>
2022-08-12 07:49:47 -0700

    linked

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,23 +1,10 @@
 #include <stdio.h>
 
 /**
- * bmain - function executed before main
- * Return: no return.
+ *xmain - a function executed before main is executed
+ *Return: no return
  */
-
-void __attribute__ ((constructor)) bmain()
-{
-	printf("You're beat! and yet, you must allow");
-	printf(",\nI bore my house upon my back!\n");
-}
-#include <stdio.h>
-
-/**
- * bmain - function executed before main
- * Return: no return.
- */
-
-void __attribute__ ((constructor)) bmain()
+void __attribute__ ((constructor)) xmain()
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
